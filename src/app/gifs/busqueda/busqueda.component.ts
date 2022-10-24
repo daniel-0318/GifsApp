77e877c9d55c726f3872e475cbc5c67d3fb68596
Daniel Ramirez <dani_0318@hotmail.com>
2022-10-23 23:56:21 -0500

    creacion del servicio para almacenar las ultimas 10 busquedas sin repetir

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,12 +10,17 @@ export class BusquedaComponent  {
 
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
 
   buscar(){
 
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    
+    if(valor.trim().length === 0){
+      return;
+    }
+    
+    this.gifsService.buscarGifs( valor );
 
     this.txtBuscar.nativeElement.value = '';
     
