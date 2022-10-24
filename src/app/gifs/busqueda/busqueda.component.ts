ecf5cb526026a0fa791911bb129027287e46ac11
Daniel Ramirez <dani_0318@hotmail.com>
2022-10-23 23:34:44 -0500

    81 creacion de modulos y componentes para la aplicacion

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent  {
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+  buscar(){
+
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+    
+  }
+
+
+
+}
